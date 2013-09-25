9295030295b3514834a9ff618136ca10aed720a6
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-25 10:33:41 -0400

    add doc; format code

--- bin/ex_03.dart
+++ bin/ex_03.dart
@@ -1,17 +1,16 @@
 void main() {
-
-  for(var i=0; i<100; i++){
-    if(i%3==0 && i%5!=0){
+  for (var i=0; i<100; i++) {
+    if (i%3==0 && i%5!=0){
       print('Fizz');
     }
-    if(i%5==0 && i%3!=0){
+    if (i%5==0 && i%3!=0){
       print('Buzz');
     }
-    if(i%3==0 && i%5==0){
+    if (i%3==0 && i%5==0){
       print('FizzBuzz');
     }
-    if(i%3!=0 && i%5!=0){
-          print('$i');
-        }
+    if (i%3!=0 && i%5!=0){
+      print('$i');
+    }
   }
 }
